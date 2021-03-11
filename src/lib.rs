9181818b5e81a647122948291bf9e1bcf6dfac54
Ales Tsurko <ales.tsurko@gmail.com>
2021-03-11 21:17:53 +0300

    Update module description

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
-//! Mosquito is a JavaScript framework for audio programming and live coding. It's written in Rust
-//! and it uses the sound compiler [Csound](https://csound.com) as the audio engine and
-//! [QuickJS](https://bellard.org/quickjs/) as the JavaScript runtime.
+//! Mosquito is an audio library for live coding, composition, sound design, DSP prototyping and
+//! any kind of audio applications.
+//! 
+//! It's written for a small and easy to learn scripting language
+//! [Koto](https://github.com/koto-lang/koto), which is easy to embed and designed for audio live
+//! coding and stuff in mind (in fact, the author of Koto is 
+//! [Ian Hobson](https://twitter.com/_hobson_), who previously worked at Ableton).
+//!
+//! Mosquito uses [Csound](https://csound.com) as a sound compiler and it's permissively licensed,
+//! allowing it to be used in proprietary software. It can be either used on it's own or embedded
+//! into your application. Notice though, that Csound is linked dynamically.
 
 #![deny(
     nonstandard_style,
